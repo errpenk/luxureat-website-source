cdf9a1d8b95e87edf4853bc3afeebccdf70fe09c
errpenk <errpenk@gmail.com>
2026-08-12 15:43:40 +0200

    Align footer groups and localize cookie fonts

--- site.config.mjs
+++ site.config.mjs
@@ -1,4 +1,4 @@
-export const assetVersion = "20260812-cookie-routing-polish-61";
+export const assetVersion = "20260812-cookie-font-alignment-62";
 
 export const contact = {
   email: "china@luxureat.com",
